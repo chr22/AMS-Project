89c19fa94fb7c3f70e43e09d28deb64a73684994
Belicosus <casperhgrd@gmail.com>
2014-02-18 21:37:10 +0100

    Added HelloArduione project
    HelloArduino project added which implements basic functionality for uart
    on ATmega32u4

---
+++ HelloArduino/HelloArduino/HelloArduino.c
@@ -0,0 +1,36 @@
+/************************************************
+*  AMS: LAB5                                    *
+*  LM75 and the Mega32 2-wire Interface         *
+*                                               *
+*  STK500 setup:                                *
+*    Header "RS232 spare" connected to RXD/TXD: *
+*      RXD = PORTD, bit0                        *
+*      TXD = PORTD, bit1                        *
+*    I2C data bus (LM75 slaves):                *
+*      SCL connected to PORTC, bit0             *
+*      SDA connected to PORTC, bit1             *
+*    Two LM75 slaves powered from STK500:       *
+*      Address no.1 = 0.                        *
+*      Address no.2 = 7.                        *
+*                                               *
+*   Henning Hargaard, February 7, 2014          *
+*************************************************/
+#include <avr/io.h>
+#define F_CPU 16000000
+#include <util/delay.h>
+#include "uart.h"
+
+int main()
+{
+	// Initialize the display
+	InitUART(9600,8);
+	while (1)
+	{
+		SendString("T1 = ");
+
+		SendInteger(123);
+		SendChar('A');
+		
+		_delay_ms(500);
+	}
+}
