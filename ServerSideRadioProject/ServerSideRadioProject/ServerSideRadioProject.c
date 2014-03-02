204293d5aa7914ad3fae5bd9c4f5c899466e8de6
GufCab <nikolaj@donald.dk>
2014-03-02 17:32:01 +0100

    Added Server side project
    Bare project, no functionality.

---
+++ ServerSideRadioProject/ServerSideRadioProject/ServerSideRadioProject.c
@@ -0,0 +1,17 @@
+/*
+ * ServerSideRadioProject.c
+ *
+ * Created: 02-03-2014 17:30:47
+ *  Author: Nikolaj
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
