f9d952d94b334b0e436513b69e6f39e7c3b162a0
Nikolaj Gustafsson <nikolaj@donald.dk>
2014-03-13 17:02:23 +0100

    Cleanup - UART
    Removed unused functions and headers
    Changed name form uart.c, uart.h to UART.c, UART.h

--- WeatherBalloon/WeatherBalloonSensor/Drivers/uart.h
+++ WeatherBalloon/WeatherBalloonSensor/Drivers/uart.h
@@ -1,22 +1,9 @@
-/**************************************
-* "uart.h":                           *
-* Header file for Mega32 UART driver. *
-* Henning Hargaard, 1/11 2011         *
-***************************************/
+
+
 #define XTAL 16000000
-//#define F_CPU XTAL
+
 
 void InitUART(unsigned long BaudRate, unsigned char DataBit);
-unsigned char CharReady();
-char ReadChar();
 void SendChar(char Ch);
-void SendString(char* String);
-void SendInteger(int Number);
-
-//Our defined functions
 int ReadCharWTimeout(char * retVal, int timeOutMs);
-int ServerResponse(char moduleId, int timeoutMS);
 
-//char readyReg;
-
-/**************************************/
